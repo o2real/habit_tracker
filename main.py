681ba433ebceb22be060e75fa31db9bf87a0315d
Kingcode <o2real@naver.com>
2024-10-24 16:14:51 +0900

    start

---
+++ main.py
@@ -0,0 +1,17 @@
+from http.client import responses
+
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "asdjflksjflksdjfl",
+    "username" : "jioh",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+
+
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
