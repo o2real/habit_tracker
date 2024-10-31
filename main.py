854019d797a46de5a51a1a587f3e70fbebec9e74
Kingcode <o2real@naver.com>
2024-10-31 14:45:26 +0900

    start

--- main.py
+++ main.py
@@ -2,18 +2,31 @@ from http.client import responses
 
 import requests
 
+USERNAME = "jioh"
+TOKEN = "asdjflksjqwer123djfl"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "asdjflksjflksdjfl",
-    "username" : "jioh",
+    "token": TOKEN,
+    "username" : USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 
 
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs "
 
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+}
+
+
+response = requests.post(url=graph_endpoint, json=graph_config)
+print(response.text)
 
