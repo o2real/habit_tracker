47d5cf861d761928a29265c4313a473b18b59688
Kingcode <o2real@naver.com>
2024-11-04 15:45:25 +0900

    start

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 
 USERNAME = "jioh"
 TOKEN = "asdjflksjqwer123djfl"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -19,14 +20,24 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs "
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Cycling Graph",
     "unit": "Km",
     "type": "float",
     "color": "ajisai",
 }
 
+headers = {
+    "X-USER-TOKEN": TOKEN,
 
-response = requests.post(url=graph_endpoint, json=graph_config)
-print(response.text)
+}
 
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": "20241104",
+    "quantity":"9.74",
+}
+
+
+requests.post(url=pixel_creation_endpoint), json=pixel_data, headers=headers
